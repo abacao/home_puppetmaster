4ff7dd5c77d4e1baf71f742e3cc9dd1a075c3e91
Andre Bacao <andre@bacao.pt>
2016-09-27 12:15:48 +0100

    removed dns

--- puppetmaster.sh
+++ puppetmaster.sh
@@ -5,12 +5,7 @@ apt -y install ntp
 service ntp restart;
 #service ntp status
 
-## 5th - Google DNS
-echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interfaces
-service networking restart
-#service networking status
-
-## 6th - Install Puppet Server
+## 5th - Install Puppet Server
 cd /tmp && wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
 apt update
@@ -18,17 +13,19 @@ apt -y install puppetserver
 service puppetserver restart
 #service puppetserver status
 
-## 7th - GIT Installation and Config
+## 6th - GIT Installation and Config
 apt update
 apt -y install git
 git config --global user.name "Andre Bacao"
 git config --global user.email andre@bacao.pt
+git config --global credential.helper 'cache --timeout=3600'
 git clone https://github.com/abacao/home_puppetmaster.git
 mv home_puppetmaster /etc
 cp /etc/puppetlabs/* /etc/home_puppetmaster -R
 sudo mv /etc/puppetlabs /etc/puppetlabs.bak
 sudo mv /etc/home_puppetmaster /etc/puppetlabs
 cd /etc/puppetlabs
+sudo git checkout master_install
 sudo git add .
 sudo git commit -a -m "added puppet default configs"
-sudo git push origin master
+sudo git push origin master_install
